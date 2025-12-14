721ecdfbb47a29786dabb2f0a8e7d260cf3d9d2f
Dmitry Kalyukov <dmitr.vk@ya.ru>
2025-12-14 15:00:25 +0200

    Draw Start button

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,37 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <button class="start-stop-button">
+  <svg viewBox="0 0 24 24" xmlns="www.w3.org" class="play-icon">
+    <!-- Path for a standard play icon (triangle) -->
+    <path fill="currentColor" d="M8 5v14l11-7z"/>
+  </svg>
+  </button>
 </template>
 
-<style scoped></style>
+<style scoped>
+  .start-stop-button {
+    background-color: #FFA500;
+    color: white;
+    border: none;
+    width: 4rem;
+    height: 4rem;
+    text-align: center;
+    justify-content: center;
+    border-radius: 2rem;
+    cursor: pointer;
+    align-items: center;
+  }
+  .start-stop-button:hover {
+    background-color: #FFB733;
+  }
+  .start-stop-button:active {
+    background-color: #E69500;
+  }
+.play-icon {
+  /* Sizing and color of the icon */
+  width: 2.5rem;
+  height: 2.5rem;
+  color: white;
+}
+</style>
--- docs/development/progress.md
+++ docs/development/progress.md
@@ -6,6 +6,8 @@ Added MkDocs to the project. Published docs site on the web.
 
 Initialized a Vue project.
 
+Added Start button (not functional for the moment).
+
 ## 2025-12-12
 
 Created a repository on GitHub.
